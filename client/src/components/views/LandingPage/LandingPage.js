89a481bd9583583b915177d97ba64c4caf780f70
yolkinthehouse <sbsbheo@gmail.com>
2021-04-27 17:26:10 +0900

    first commit

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FaCode } from "react-icons/fa";
+
+function LandingPage() {
+    return (
+        <>
+            <div className="app">
+                <FaCode style={{ fontSize: '4rem' }} /><br />
+                <span style={{ fontSize: '2rem' }}>청첩장공사중</span>
+            </div>
+        </>
+    )
+}
+
+export default LandingPage
---
+++ server/config/prod.js
@@ -0,0 +1,3 @@
+module.exports ={
+    mongoURI:'mongodb+srv://sangbeom:hsb4335gjgj@boilerplate.0baow.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
